a47b37897b4a6281076d11d8e4ac6e625773b913
accacca44 <47551816+accacca44@users.noreply.github.com>
2019-02-13 09:34:03 +0200

    Add files via upload

---
+++ tombok/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int tomb[n];
+    for(int i=0;i<n;i++){
+        cin>>tomb[i];
+    }
+     for(int i=0;i<n;i++){
+        cout<<tomb[i];
+    }
+
+
+    return 0;
+}
